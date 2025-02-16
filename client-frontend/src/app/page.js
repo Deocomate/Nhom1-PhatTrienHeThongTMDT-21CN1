bd29b1a41d68f57034764d972ce82cf19e92268d
JFry8 <191094439+JFry8@users.noreply.github.com>
2025-02-16 20:42:02 +0700

    Thêm carousel

--- client-frontend/src/app/page.js
+++ client-frontend/src/app/page.js
@@ -1,25 +1,67 @@
 "use client";
+
 import ProductCard from "@/components/ProductCard";
-import ProductCardButton from "@/components/ProductCardButton";
-import NotificationButton from "@/components/NotificationButton";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 
 export default function Home() {
     return (
-        <>
-            <div className="">
-                <div className="max-w-screen-xl flex flex-wrap items-center  mx-auto px-4 pt-4">
-                    <h1 className="text-xl font-bold">Top bán chạy toàn quốc</h1>
-                </div>
-                <div className="max-w-screen-xl flex flex-wrap items-center  mx-auto p-4 gap-8">
-                    <ProductCard
-                        title={"Cao dán Salonpas Pain Relief Patch 7cmx10cm giảm đau vai, đau cổ, đau lưng, đau khớp (hộp 3 miếng)"}
-                        price={"30.000 đ/Hộp"}
-                        favor={"Yêu thích 51.8k"}
-                        sale={"Đã bán 8.4k"}
-                    />
+        <div className="w-full flex flex-col items-center">
+            {/*<div className="max-w-screen-xl w-full px-4 pt-4">*/}
 
-                </div>
+            {/*</div>*/}
+
+            {/* Carousel Container */}
+            <div className="max-w-screen-xl w-full p-4">
+                <h1 className="text-xl font-bold">Top bán chạy toàn quốc</h1>
+                <Carousel className="w-full">
+                    <CarouselContent className="flex">
+                        <CarouselItem className="flex gap-8">
+                            <ProductCard
+                                title={"Cao dán Salonpas Pain Relief Patch 7cmx10cm giảm đau vai, đau cổ, đau lưng, đau khớp (hộp 3 miếng)"}
+                                price={"30.000 đ/Hộp"}
+                                favor={"Yêu thích 51.8k"}
+                                sale={"Đã bán 8.4k"}
+                            />
+                            <ProductCard
+                                title={"Cao dán Salonpas Pain Relief Patch 7cmx10cm giảm đau vai, đau cổ, đau lưng, đau khớp (hộp 3 miếng)"}
+                                price={"30.000 đ/Hộp"}
+                                favor={"Yêu thích 51.8k"}
+                                sale={"Đã bán 8.4k"}
+                            />
+                            <ProductCard
+                                title={"Cao dán Salonpas Pain Relief Patch 7cmx10cm giảm đau vai, đau cổ, đau lưng, đau khớp (hộp 3 miếng)"}
+                                price={"30.000 đ/Hộp"}
+                                favor={"Yêu thích 51.8k"}
+                                sale={"Đã bán 8.4k"}
+                            />
+                            <ProductCard
+                                title={"Cao dán Salonpas Pain Relief Patch 7cmx10cm giảm đau vai, đau cổ, đau lưng, đau khớp (hộp 3 miếng)"}
+                                price={"30.000 đ/Hộp"}
+                                favor={"Yêu thích 51.8k"}
+                                sale={"Đã bán 8.4k"}
+                            />
+                            <ProductCard
+                                title={"Cao dán Salonpas Pain Relief Patch 7cmx10cm giảm đau vai, đau cổ, đau lưng, đau khớp (hộp 3 miếng)"}
+                                price={"30.000 đ/Hộp"}
+                                favor={"Yêu thích 51.8k"}
+                                sale={"Đã bán 8.4k"}
+                            />
+
+                        </CarouselItem>
+                        <CarouselItem>
+                            <ProductCard
+                                title={"Cao dán Salonpas Pain Relief Patch 7cmx10cm giảm đau vai, đau cổ, đau lưng, đau khớp (hộp 3 miếng)"}
+                                price={"30.000 đ/Hộp"}
+                                favor={"Yêu thích 51.8k"}
+                                sale={"Đã bán 8.4k"}
+                            />
+                        </CarouselItem>
+                        <CarouselItem>...</CarouselItem>
+                    </CarouselContent>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                </Carousel>
             </div>
-        </>
-    )
+        </div>
+    );
 }
