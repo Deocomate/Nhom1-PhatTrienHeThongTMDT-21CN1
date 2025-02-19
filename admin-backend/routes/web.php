dcdef1c3843852c638f2d50d6c77cec7f2c17b76
Deocomate <deocomate@gmail.com>
2025-02-20 00:01:30 +0700

    update

--- admin-backend/routes/web.php
+++ admin-backend/routes/web.php
@@ -5,10 +5,8 @@ use App\Http\Controllers\Admin\{AdminController,
     TourSystem\ContactController,
     TourSystem\DestinationController,
     TourSystem\TourController
-    
+
 };
-
-
 
 
 use App\Http\Controllers\Admin\PharmacySystem\{
@@ -27,8 +25,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/seeder', [SeederController::class, "index"]);
 
 // Client
-Route::get('/', [ClientHomeController::class, "index"])->name("client.index");
-Route::get('/', [ClientHomeController::class, "index"])->name("client.homepage");
+Route::get('/', function () {
+    return to_route("admin.index");
+})->name("client.index");
+//Route::get('/', [ClientHomeController::class, "index"])->name("client.homepage");
 
 // Admin
 
@@ -45,17 +45,14 @@ Route::prefix('admin')->name("admin.")->middleware(AuthenticationMiddleware::cla
 
     // Các chức năng chính dựa theo module (Đây là các module mẫu)
     // Route::resource("category", CategoryController::class);
-    Route::resource("destination", DestinationController::class);
-    Route::resource("tour", TourController::class);
-    Route::resource("contact", ContactController::class)
-        ->except("create", "show", "edit", "destroy", "store");
-
-
+    // Route::resource("destination", DestinationController::class);
+    // Route::resource("tour", TourController::class);
+    // Route::resource("contact", ContactController::class)
+    //     ->except("create", "show", "edit", "destroy", "store");
 
     // BlogCategory
     Route::resource("blogcategory", BlogCategoryController::class);
     Route::resource("blog", BlogController::class);
-
     Route::resource("category", CategoryController::class);
 
 });
