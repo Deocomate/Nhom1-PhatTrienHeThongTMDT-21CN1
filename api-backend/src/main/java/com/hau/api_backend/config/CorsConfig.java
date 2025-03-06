bdac126e3bf845cd467e837195283b136a6155ae
Deocomate <deocomate@gmail.com>
2025-03-06 16:09:17 +0700

    update

--- api-backend/src/main/java/com/hau/api_backend/config/CorsConfig.java
+++ api-backend/src/main/java/com/hau/api_backend/config/CorsConfig.java
@@ -9,11 +9,10 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With")
-                .allowCredentials(true)
-                .maxAge(3600);
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
