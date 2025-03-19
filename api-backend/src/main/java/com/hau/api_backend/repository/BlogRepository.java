8debac819945255d756a58f8f47e4500e71528bb
lmao1166 <tuantt3010@gmail.com>
2025-03-19 15:01:00 +0700

    fix search Blog

--- api-backend/src/main/java/com/hau/api_backend/repository/BlogRepository.java
+++ api-backend/src/main/java/com/hau/api_backend/repository/BlogRepository.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 public interface BlogRepository extends JpaRepository<Blog, Integer> {
     Optional<Blog> findBlogBySlug(String slug);
     Page<Blog> findAll(Pageable pageable);
-    @Query("SELECT b FROM Blog b WHERE LOWER(b.title) LIKE LOWER(CONCAT(:title, '%'))")
+    @Query("SELECT b FROM Blog b WHERE LOWER(b.title) LIKE LOWER(CONCAT('%', :title, '%'))")
     Optional<Page<Blog>> searchByTitle(@Param("title") String title, Pageable pageable);
 
 
